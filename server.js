a9d740c5839a388f956c0b81c5e3f0e079ecdda4
Meisseu <m.mara@ecole-ipssi.net>
2025-02-19 10:43:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ app.use('/api/customers', customerRoutes);
 app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+
